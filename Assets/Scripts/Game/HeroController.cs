5a4216056c4544e637f58fce4da785a61644a45e
ReWriteGame <ReWriteGame>
2022-12-18 14:04:13 +0200

    fix animation fly and add skybox

---
+++ Assets/Scripts/Game/HeroController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1;
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + moveSpeed / 1000);
+        }
+    }
+}
